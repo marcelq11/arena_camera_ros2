a7728fb400648f0171d3ef6432cdc40754e4ca36
Marcel <marceq38@gmail.com>
2024-06-24 12:11:27 +0200

    initial commit camera_launch

---
+++ ros2_ws/src/camera_launch/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marcel',
+    maintainer_email='marceq38@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
