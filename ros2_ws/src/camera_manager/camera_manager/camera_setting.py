9d1fd3a26b473eaa146f8a8f2aedf0a8212070d4
opszalek <dawidc4@hotmail.com>
2024-06-24 15:15:41 +0200

    changed msg type

--- ros2_ws/src/camera_manager/camera_manager/camera_setting.py
+++ ros2_ws/src/camera_manager/camera_manager/camera_setting.py
@@ -1,22 +1,23 @@
 # camera_publisher.py
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 from .setting_gui import CameraGUI
-
+from camera_msg.msg import CameraSettings
 class CameraPublisher(Node):
     def __init__(self):
         super().__init__('camera_publisher')
-        self.publisher_ = self.create_publisher(String, 'camera_params', 10)
+        self.publisher_ = self.create_publisher(CameraSettings, 'camera_params', 10)
         self.gui = CameraGUI(self.update_settings)  # Przekazanie callback
 
     def update_settings(self):
         gain, exposure, gamma, pixel_format = self.gui.get_settings()
-        message = f"GAIN: {gain}, EXPOSURE: {exposure}, GAMMA: {gamma}, PIXEL FORMAT: {pixel_format}"
-        msg = String()
-        msg.data = message
+        msg = CameraSettings()
+        msg.exposure_time = exposure
+        msg.gamma = gamma
+        msg.gain = gain
+        msg.pixelformat = pixel_format
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: {msg.data}')
+        self.get_logger().info(f'Publishing: {msg}')
 
     def run(self):
         rclpy.spin_once(self, timeout_sec=0.1)
