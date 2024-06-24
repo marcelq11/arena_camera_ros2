bcd05685e559c775566c63e4efff82106dd2c90d
Marcel <marceq38@gmail.com>
2024-06-24 13:08:09 +0200

    arena camera node params and launch added

---
+++ ros2_ws/src/arena_camera_node/launch/start_arena_camera.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('arena_camera_node'),
+        'config',
+        'arena_camera_params.yaml'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='arena_camera_node',
+            executable='start',
+            name='arena_camera_node',
+            parameters=[config]
+        )
+    ])
