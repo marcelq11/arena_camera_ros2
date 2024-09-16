fccba3fcc3ab865f4435ab06dd7a4349573fbdaa
opszalek <dawidc4@hotmail.com>
2024-09-16 17:38:11 +0200

    Multithreading and showing output

--- ros2_ws/src/sign_recognition_node/sign_recognition_node/sign_recognition_node_main.py
+++ ros2_ws/src/sign_recognition_node/sign_recognition_node/sign_recognition_node_main.py
@@ -1,4 +1,5 @@
 import rclpy
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
@@ -6,26 +7,32 @@ from cv_bridge import CvBridge, CvBridgeError
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 import cv2
 import os
-
+import time
+import threading
+import queue
 from image_processing.image_main import SignTextRecognitionSystem
 
-qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
+qos_profile = QoSProfile(depth=100, reliability=ReliabilityPolicy.RELIABLE)
 
 class SignTextRecognitionNode(Node):
     def __init__(self):
         super().__init__('sign_text_recognition_node')
 
         self.bridge = CvBridge()
+        self.image_queue = queue.Queue()
 
         models_path = os.environ.get('MODELS_PATH')
 
+        self.enable_preview = True
+
         self.sign_text_recognition_system = SignTextRecognitionSystem(
             models_path=models_path,
-            model='yolov8',
-            save_results=True,
-            show_signs=True,
+            model_type='yolov8n',
+            save_results=False,
+            show_signs=False,
             show_images=False,
             save_frames=False,
+            enable_preview=self.enable_preview,
             ocr='paddle'
         )
 
@@ -37,32 +44,76 @@ class SignTextRecognitionNode(Node):
         self.subscription
 
         self.publisher = self.create_publisher(String, 'sign_text_output', 10)
+        self.previous_time = time.time()
+        self.sum_time = 0
+        self.frame_count = 0
+
+
+        self.display_thread = threading.Thread(target=self.display_images)
+        self.display_thread.daemon = True
+        self.display_thread.start()
 
     def image_callback(self, msg):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
             self.get_logger().error(f'Error converting image: {e}')
             return
-
-        self.sign_text_recognition_system.process_frame(cv_image)
         
+ 
+        if self.enable_preview:
+            signs, text, image = self.sign_text_recognition_system.process_frame(cv_image)
+        
+  
         text_output = "Sample Text Output"
         msg = String()
         msg.data = text_output
         self.publisher.publish(msg)
 
+      
+        current_time = time.time()
+        frame_duration = current_time - self.previous_time
+
+        #FPS calculation FOR TESTING
+        # self.sum_time += frame_duration
+        # self.frame_count += 1
+        # if frame_duration > 0:
+        #     fps = 1.0 / frame_duration
+        #     avg_fps = self.frame_count / self.sum_time
+        #     self.get_logger().info(f'FPS: {fps:.2f}, Average FPS: {avg_fps:.2f}')
+
+        self.previous_time = current_time
+
+        if self.enable_preview:
+            image = cv2.resize(image, (640, 640))
+            self.image_queue.put(image)
+
+    def display_images(self):
+        while rclpy.ok():
+            try:
+                cv_image = self.image_queue.get(timeout=1)
+                if cv_image is not None:
+                    cv2.imshow("Image", cv_image)
+                    cv2.waitKey(1)
+            except queue.Empty:
+                continue
+
 def main(args=None):
     rclpy.init(args=args)
     node = SignTextRecognitionNode()
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    
     try:
-        rclpy.spin(node)
+        executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         node.destroy_node()
         rclpy.shutdown()
-        cv2.destroyAllWindows()
+
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
