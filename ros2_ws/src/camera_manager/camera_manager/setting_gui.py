197de85c1ac53dc48c175e0c9106ceda6a259de0
opszalek <dawidc4@hotmail.com>
2024-09-23 13:21:38 +0200

    Refactored GUI-settings tab

--- ros2_ws/src/camera_manager/camera_manager/setting_gui.py
+++ ros2_ws/src/camera_manager/camera_manager/setting_gui.py
@@ -1,234 +1,188 @@
-# camera_gui.py
 import tkinter as tk
-import yaml
-import os
-from tkinter import messagebox
+from tkinter import ttk
 
 
 class CameraGUI:
-    def __init__(self, update_callback, error_func, parameters_path=''):
-        #Some predefined parameters
-        self.pixel_format = 'bayer_rggb8'
-        self.gain = 5.0
-        self.exposure = 10000.0
-        self.gamma = 1.0
-        self.width = 2448
-        self.height = 2048
-        self.recording = 0
-
-        self.parameters_path = parameters_path
-        self.error_callback = error_func
-        self.update_callback = update_callback
-        self.load_parameters()
-
-        self.gamma_step = 0.1
-        self.gain_step = 1.0
-        self.exposure_step = 1000.0
-        self.pixel_format_list = ["rgb8", "rgba8", "rgb16", "rgba16", "bgr8", "bgra8", "bgr16", "bgra16",
-                                  "mono8", "mono16", "bayer_rggb8", "bayer_bggr8", "bayer_gbrg8", "bayer_grbg8",
-                                  "bayer_rggb16", "bayer_bggr16", "bayer_gbrg16", "bayer_grbg16", "yuv422"]
-
+    def __init__(self):
         self.root = tk.Tk()
-        self.root.title("Camera Settings")
-        self.root.geometry('550x150+500+500')
+        self.root.title("Camera Manager")
+        self.root.geometry('600x400+500+500')
 
-        tk.Label(self.root, text="Gain").grid(row=0, column=1)
-        self.gain_entry = tk.Entry(self.root)
-        self.gain_entry.grid(row=1, column=1)
-        self.gain_entry.config(width=7)
-        self.gain_entry.insert(0, str(self.gain))
+        self.notebook = ttk.Notebook(self.root)
 
-        self.increase_Gain_button = tk.Button(self.root, text="+",
-                                              command=lambda: self.change_param("gain", self.gain_step))
-        self.increase_Gain_button.grid(row=1, column=2)
-        self.decrease_Gain_button = tk.Button(self.root, text="-",
-                                              command=lambda: self.change_param("gain", -self.gain_step))
-        self.decrease_Gain_button.grid(row=1, column=0)
+        self.camera_settings_tab = ttk.Frame(self.notebook)
+        self.system_manager_tab = ttk.Frame(self.notebook)
 
-        tk.Label(self.root, text="Exposure").grid(row=0, column=4)
-        self.exposure_entry = tk.Entry(self.root)
-        self.exposure_entry.grid(row=1, column=4)
-        self.exposure_entry.config(width=7)
-        self.exposure_entry.insert(0, str(self.exposure))
+        self.notebook.add(self.system_manager_tab, text="System Manager")
+        self.notebook.add(self.camera_settings_tab, text="Camera Settings")
 
-        self.increase_Exposure_button = tk.Button(self.root, text="+",
-                                                  command=lambda: self.change_param("exposure", self.exposure_step))
-        self.increase_Exposure_button.grid(row=1, column=5)
-        self.decrease_Exposure_button = tk.Button(self.root, text="-",
-                                                  command=lambda: self.change_param("exposure", -self.exposure_step))
-        self.decrease_Exposure_button.grid(row=1, column=3)
+        self.notebook.pack(expand=1, fill='both')
 
-        tk.Label(self.root, text="Gamma").grid(row=0, column=7)
-        self.gamma_entry = tk.Entry(self.root)
-        self.gamma_entry.grid(row=1, column=7)
-        self.gamma_entry.config(width=7)
-        self.gamma_entry.insert(0, str(self.gamma))
+        self.create_camera_settings_tab()
 
-        self.increase_Gamma_button = tk.Button(self.root, text="+",
-                                               command=lambda: self.change_param("gamma", self.gamma_step))
-        self.increase_Gamma_button.grid(row=1, column=8)
-        self.decrease_Gamma_button = tk.Button(self.root, text="-",
-                                               command=lambda: self.change_param("gamma", -self.gamma_step))
-        self.decrease_Gamma_button.grid(row=1, column=6)
+        self.create_system_manager_tab()
 
-        self.pixel_format_var = tk.StringVar(self.root)
-        self.pixel_format_var.set(self.pixel_format)
-        tk.Label(self.root, text="Pixel Format").grid(row=2, column=7)
-        self.pixel_format_menu = tk.OptionMenu(self.root, self.pixel_format_var, *self.pixel_format_list)
-        self.pixel_format_menu.grid(row=3, column=7)
+    def create_camera_settings_tab(self):
+        tk.Label(self.camera_settings_tab, text="       Auto Exposure Time Limits:").grid(row=0, column=1, padx=10, pady=5)
+        tk.Label(self.camera_settings_tab, text="Upper:").grid(row=1, column=2, padx=10, pady=5,
+                                                                                   sticky="w")
+        tk.Label(self.camera_settings_tab, text="Lower:").grid(row=1, column=0, padx=10, pady=5,
+                                                                                   sticky="w")
 
-        tk.Label(self.root, text="Width").grid(row=2, column=1)
-        self.width_entry = tk.Entry(self.root)
-        self.width_entry.grid(row=3, column=1)
-        self.width_entry.config(width=7)
-        self.width_entry.insert(0, str(self.width))
+        self.exposure_limit_lower_slider = tk.Scale(self.camera_settings_tab, from_=30, to=3000, orient=tk.HORIZONTAL,
+                                                    command=self.update_lower_exposure_limit)
+        self.exposure_limit_lower_slider.grid(row=1, column=1, columnspan=1, padx=10, pady=5)
 
-        tk.Label(self.root, text="Height").grid(row=2, column=4)
-        self.height_entry = tk.Entry(self.root)
-        self.height_entry.grid(row=3, column=4)
-        self.height_entry.config(width=7)
-        self.height_entry.insert(0, str(self.height))
+        self.exposure_limit_upper_slider = tk.Scale(self.camera_settings_tab, from_=1000, to=30000,
+                                                    orient=tk.HORIZONTAL, command=self.update_upper_exposure_limit)
+        self.exposure_limit_upper_slider.grid(row=1, column=3, columnspan=3, padx=10, pady=5)
 
-        self.recording_button = tk.Button(self.root, text="Start Recording", command=self.toggle_recording)
-        self.recording_button.grid(row=4, column=1, columnspan=3)
-        self.recording_button.config(bg='green')
-        self.recording_button.config(activebackground=self.recording_button.cget('background'))
+        tk.Label(self.camera_settings_tab, text="       Auto Gain Limits:").grid(row=2, column=1, padx=10,
+                                                                                          pady=5)
+        tk.Label(self.camera_settings_tab, text="Upper:").grid(row=3, column=2, padx=10, pady=5,
+                                                               sticky="w")
+        tk.Label(self.camera_settings_tab, text="Lower:").grid(row=3, column=0, padx=10, pady=5,
+                                                               sticky="w")
 
+        self.gain_limit_lower_slider = tk.Scale(self.camera_settings_tab, from_=0, to=30, orient=tk.HORIZONTAL, command=self.update_lower_gain_limit)
+        self.gain_limit_lower_slider.grid(row=3, column=1, columnspan=1, padx=10, pady=5)
 
-        tk.Button(self.root, text="Update Settings", command=update_callback).grid(row=4, column=4)
-        self.update_entries()
+        self.gain_limit_upper_slider = tk.Scale(self.camera_settings_tab, from_=0, to=30, orient=tk.HORIZONTAL, command=self.update_upper_gain_limit)
+        self.gain_limit_upper_slider.grid(row=3, column=3, columnspan=3, padx=10, pady=5)
 
-    def toggle_recording(self):
-        if self.recording:
-            self.recording = 0
-            self.recording_button.config(text="Start Recording")
-            self.recording_button.config(bg='green')
-        else:
-            self.recording = 1
-            self.recording_button.config(text="Stop Recording")
-            self.recording_button.config(bg='red')
-        self.recording_button.config(activebackground=self.recording_button.cget('background'))
-        self.update_callback()
+        tk.Label(self.camera_settings_tab, text="Target Brightness:").grid(row=4, column=0, padx=10, pady=5, sticky="w")
+        self.brightness_slider = tk.Scale(self.camera_settings_tab, from_=0, to=120, orient=tk.HORIZONTAL)
+        self.brightness_slider.grid(row=4, column=1, columnspan=3, padx=10, pady=5)
 
+        tk.Label(self.camera_settings_tab, text="ROI Width:").grid(row=6, column=0, padx=10, pady=5, sticky="w")
+        self.roi_width_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.roi_width_entry.grid(row=6, column=1, padx=10, pady=5)
+        self.roi_width_entry.bind("<FocusOut>", lambda event: self.update_roi_width(self.roi_width_entry.get()))
 
-    def load_parameters_from_file(self):
-        if not os.path.exists(self.parameters_path):
-            self.error_callback(f"Plik z parametrami o ścieżce {self.parameters_path} nie istnieje.\n"
-                                f"Utwórz plik z parametrami lub podaj poprawną ścieżkę.\n"
-                                f"W przypadku braku pliku z parametrami zostaną użyte domyślne wartości.")
-            return None
+        tk.Label(self.camera_settings_tab, text="ROI Height:").grid(row=6, column=2, padx=10, pady=5, sticky="w")
+        self.roi_height_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.roi_height_entry.grid(row=6, column=3, padx=10, pady=5)
+        self.roi_height_entry.bind("<FocusOut>", lambda event: self.update_roi_height(self.roi_height_entry.get()))
 
-        try:
-            with open(self.parameters_path, 'r') as file:
-                camera_parameters = yaml.load(file, Loader=yaml.FullLoader)
-                return camera_parameters
-        except Exception as e:
-            self.error_callback(f"Błąd podczas ładowania pliku z parametrami: {e}")
-            return None
+        tk.Label(self.camera_settings_tab, text="ROI Offset X:").grid(row=7, column=0, padx=10, pady=5, sticky="w")
+        self.roi_offset_x_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.roi_offset_x_entry.grid(row=7, column=1, padx=10, pady=5)
+        self.roi_offset_x_entry.bind("<FocusOut>",
+                                     lambda event: self.update_roi_offset_x(self.roi_offset_x_entry.get()))
 
-    def load_parameters(self):
-        parameters = self.load_parameters_from_file()
-        if parameters is not None:
-            camera_parameters = parameters['arena_camera_node']['ros__parameters']
-            self.gain = camera_parameters['gain']
-            self.exposure = camera_parameters['exposure_time']
-            self.gamma = camera_parameters['gamma']
-            self.pixel_format = camera_parameters['pixelformat']
-            self.width = camera_parameters['width']
-            self.height = camera_parameters['height']
+        tk.Label(self.camera_settings_tab, text="ROI Offset Y:").grid(row=7, column=2, padx=10, pady=5, sticky="w")
+        self.roi_offset_y_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.roi_offset_y_entry.grid(row=7, column=3, padx=10, pady=5)
+        self.roi_offset_y_entry.bind("<FocusOut>",
+                                     lambda event: self.update_roi_offset_y(self.roi_offset_y_entry.get()))
 
-    def save_parameters(self):
-        parameters = self.load_parameters_from_file()
-        if parameters is None:
-            return
-        camera_parameters = parameters['arena_camera_node']['ros__parameters']
-        camera_parameters['gain'] = self.gain
-        camera_parameters['exposure_time'] = self.exposure
-        camera_parameters['gamma'] = self.gamma
-        camera_parameters['pixelformat'] = self.pixel_format
-        camera_parameters['width'] = self.width
-        camera_parameters['height'] = self.height
-        with open(self.parameters_path, 'w') as file:
-            yaml.dump(parameters, file)
+        tk.Label(self.camera_settings_tab, text="Image Width:").grid(row=5, column=0, padx=10, pady=5, sticky="w")
+        self.width_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.width_entry.grid(row=5, column=1, padx=10, pady=5)
+        self.width_entry.bind("<FocusOut>", lambda event: self.update_image_width(self.width_entry.get()))
+
+        tk.Label(self.camera_settings_tab, text="Image Height:").grid(row=5, column=2, padx=10, pady=5, sticky="w")
+        self.height_entry = tk.Entry(self.camera_settings_tab, width=10)
+        self.height_entry.grid(row=5, column=3, padx=10, pady=5)
+        self.height_entry.bind("<FocusOut>", lambda event: self.update_image_height(self.height_entry.get()))
+
+        tk.Button(self.camera_settings_tab, text="Update Settings", command=self.update_settings).grid(row=8, column=0, columnspan=4, pady=10)
+
+    def update_lower_exposure_limit(self, val):
+        lower_value = int(val)
+        if lower_value >= self.exposure_limit_upper_slider.get():
+            self.exposure_limit_upper_slider.set(lower_value)
+
+    def update_upper_exposure_limit(self, val):
+        upper_value = int(val)
+        if upper_value <= self.exposure_limit_lower_slider.get():
+            self.exposure_limit_lower_slider.set(upper_value)
+
+    def update_lower_gain_limit(self, val):
+        lower_gain = int(val)
+        if lower_gain >= self.gain_limit_upper_slider.get():
+            self.gain_limit_upper_slider.set(lower_gain)
+
+    def update_upper_gain_limit(self, val):
+        upper_gain = int(val)
+        if upper_gain <= self.gain_limit_lower_slider.get():
+            self.gain_limit_lower_slider.set(upper_gain)
+
+    def update_image_width(self, val):
+        width = abs(int(val))
+        max_width = 2448
+        if width > max_width:
+            self.width_entry.delete(0, tk.END)
+            self.width_entry.insert(0, str(max_width))
+
+    def update_image_height(self, val):
+        height = abs(int(val))
+        max_height = 2048
+        if height > max_height:
+            self.height_entry.delete(0, tk.END)
+            self.height_entry.insert(0, str(max_height))
+
+    def update_roi_width(self, val):
+        width = int(val)
+        max_width = int(self.width_entry.get())
+        if width > max_width:
+            self.roi_width_entry.delete(0, tk.END)
+            self.roi_width_entry.insert(0, str(max_width))
+        elif width > max_width - int(self.roi_offset_x_entry.get()):
+            self.roi_offset_x_entry.delete(0, tk.END)
+            self.roi_offset_x_entry.insert(0, str(max_width - width))
+
+    def update_roi_height(self, val):
+        height = int(val)
+        max_height = int(self.height_entry.get())
+        if height > max_height:
+            self.roi_height_entry.delete(0, tk.END)
+            self.roi_height_entry.insert(0, str(max_height))
+        elif height > max_height - int(self.roi_offset_y_entry.get()):
+            self.roi_offset_y_entry.delete(0, tk.END)
+            self.roi_offset_y_entry.insert(0, str(max_height - height))
+
+    def update_roi_offset_x(self, val):
+        offset_x = int(val)
+        max_offset_x = int(self.width_entry.get())
+        if offset_x > max_offset_x:
+            self.roi_offset_x_entry.delete(0, tk.END)
+            self.roi_offset_x_entry.insert(0, str(max_offset_x))
+        elif offset_x > max_offset_x - int(self.roi_width_entry.get()):
+            self.roi_width_entry.delete(0, tk.END)
+            self.roi_width_entry.insert(0, str(max_offset_x - offset_x))
+
+    def update_roi_offset_y(self, val):
+        offset_y = int(val)
+        max_offset_y = int(self.height_entry.get())
+        if offset_y > max_offset_y:
+            self.roi_offset_y_entry.delete(0, tk.END)
+            self.roi_offset_y_entry.insert(0, str(max_offset_y))
+        elif offset_y > max_offset_y - int(self.roi_height_entry.get()):
+            self.roi_height_entry.delete(0, tk.END)
+            self.roi_height_entry.insert(0, str(max_offset_y - offset_y))
+
+#-------------------------------------------------------------------------
+    def create_system_manager_tab(self):
+        tk.Button(self.system_manager_tab, text="Start System", command=self.start_system).grid(row=0, column=0,
+                                                                                                padx=10, pady=10)
+
+        tk.Button(self.system_manager_tab, text="Stop System", command=self.stop_system).grid(row=1, column=0, padx=10,
+                                                                                              pady=10)
+
+    def update_settings(self):
+        print("Settings updated!")
+
+    def start_system(self):
+        print("System started!")
+
+    def stop_system(self):
+        print("System stopped!")
 
     def run(self):
         self.root.mainloop()
 
-    def update_entries(self):
-        self.gain_entry.delete(0, tk.END)
-        self.gain_entry.insert(0, str(self.gain))
 
-        self.exposure_entry.delete(0, tk.END)
-        self.exposure_entry.insert(0, str(self.exposure))
-
-        self.gamma_entry.delete(0, tk.END)
-        self.gamma_entry.insert(0, str(self.gamma))
-
-        self.pixel_format_var.set(self.pixel_format)
-
-        self.width_entry.delete(0, tk.END)
-        self.width_entry.insert(0, str(self.width))
-
-        self.height_entry.delete(0, tk.END)
-        self.height_entry.insert(0, str(self.height))
-
-    def change_param(self, param_name, value):
-        current_value = getattr(self, param_name)
-        new_value = current_value + value
-        setattr(self, param_name, new_value)
-        self.save_parameters()
-        self.update_entries()
-        self.update_callback()
-
-    def check_is_restart_needed(self, width, height, pixel_format):
-        if (self.pixel_format != pixel_format or
-                self.width != width or self.height != height):
-            return True
-        return False
-
-    def handle_parameters(self):
-        exposure, gamma, gain, width, height, pixel_format = self.get_parameters()
-        if exposure <= 50:
-            exposure = 50.0
-        elif exposure >= 47000:
-            exposure = 47000.0
-        if gamma <= 0.2:
-            gamma = 0.2
-        elif gamma >= 1.9:
-            gamma = 1.9
-        if gain < 0:
-            gain = 0.0
-        elif gain >= 48:
-            gain = 48.0
-        if width <= 200:
-            width = self.width
-        elif width >= 2448:
-            width = 2448
-        if height <= 200:
-            height = self.height
-        elif height >= 2048:
-            height = 2048
-        self.exposure = exposure
-        self.gamma = gamma
-        self.gain = gain
-        if self.check_is_restart_needed(width, height, pixel_format):
-            answer = messagebox.askquestion("Restart",
-                                            "Zmiana width/height/pixel format wymaga restartu. Czy kontynuować?")
-            if answer == 'yes':
-                self.pixel_format = pixel_format
-                self.width = width
-                self.height = height
-        self.update_entries()
-
-    def get_parameters(self):
-        exposure = float(self.exposure_entry.get())
-        gamma = float(self.gamma_entry.get())
-        gain = float(self.gain_entry.get())
-        pixel_format = str(self.pixel_format_var.get())
-        width = int(self.width_entry.get())
-        height = int(self.height_entry.get())
-        return exposure, gamma, gain, width, height, pixel_format
-
-    def return_params(self):
-        self.handle_parameters()
-        return self.gain, self.exposure, self.gamma, self.pixel_format, self.width, self.height, self.recording
+if __name__ == "__main__":
+    gui = CameraGUI()
+    gui.run()
