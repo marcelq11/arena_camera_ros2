540b2ee8c4e07d78d5c5bf7adaaab465db916d7a
marcelq11 <marcelq38@gmail.com>
2024-11-11 21:55:11 +0100

    windows environment setup files

--- setup_enviroment_win.bat
+++ setup_enviroment_win.bat
@@ -6,21 +6,30 @@ REM Set base and destination directories
 set "BASE_DIR=%cd%"
 set "DEST_DIR=%BASE_DIR%\ros2_ws\src\sign_recognition_node\sign_recognition_node\Distance_Sign_Recognition"
 
-REM Export the PYTHONPATH
-set "PYTHONPATH=%PYTHONPATH%;%DEST_DIR%"
-set "PYTHONPATH=%PYTHONPATH%;%DEST_DIR%\image_processing"
-set "PYTHONPATH=%PYTHONPATH%;%DEST_DIR%\utils"
+REM Export the PYTHONPATH only if it hasn't been set already
+echo %PYTHONPATH% | find /I "%DEST_DIR%" >nul
+if errorlevel 1 (
+    set "PYTHONPATH=%DEST_DIR%"
+    set "PYTHONPATH=%PYTHONPATH%;%DEST_DIR%\image_processing"
+    set "PYTHONPATH=%PYTHONPATH%;%DEST_DIR%\utils"
+) else (
+    echo PYTHONPATH is already set, skipping
+)
 
 REM Navigate to the Models directory and run model download script
 cd "%DEST_DIR%\Models"
 
-REM call download_models.bat
+call download_models.bat
 
 REM Return to the base directory
 cd %BASE_DIR%
 
-REM Set the MODELS_PATH environment variable
-set "MODELS_PATH=%DEST_DIR%\Models"
+REM Set the MODELS_PATH environment variable only if it hasn't been set already
+if "%MODELS_PATH%"=="" (
+    set "MODELS_PATH=%DEST_DIR%\Models"
+) else (
+    echo MODELS_PATH is already set, skipping
+)
 
 REM Navigate to ros2_ws directory
 cd ros2_ws || (echo "Failed to change directory to ros2_ws" & exit /b 1)
